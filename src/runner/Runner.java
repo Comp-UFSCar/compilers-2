78fa0e4cd193ebdea9a7fbee0c3f615fecb5930a
Lucas David <lucasolivdavid@gmail.com>
2014-09-06 17:41:53 -0300

    Implement MultipleAnalysisRunner and insert internal doc

--- src/runner/Runner.java
+++ src/runner/Runner.java
@@ -5,36 +5,39 @@ import infrastructure.ErrorListener;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.PrintWriter;
-import java.util.Calendar;
-import java.util.Date;
 import laparser.LaLexer;
 import laparser.LaParser;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 
+/**
+ * Runs the LaParser on a inputFile and outputs the result in an outputFile.
+ * 
+ * This is the main class of the LaParser project. Therefore, the method
+ * Runner.main(String[] args) will be called by the evaluator.
+ * 
+ * @author Lucas
+ */
 public class Runner {
-    
-    public static void main(String[] args) throws Exception {
-        
-        String inputFile = "D:/Repositories/Compilers-2-assignment-1/src/testcases/1.arquivos_com_erros_sintaticos/entrada/1-algoritmo_2-2_apostila_LA_1_erro_linha_3_acusado_linha_10.txt",
-               outputFile = "D:/Repositories/Compilers-2-assignment-1/src/reports/" + System.nanoTime() + ".txt";
-        
-        if (args.length > 0) {
-            inputFile = args[0];
-            
-            if (args.length > 1) {
-                outputFile = args[1];
-            }
-        }
+
+    /**
+     * Runs the LaParser on a file that contains a LA source-code.
+     * 
+     * @param inputFile name of the LA source-code file
+     * @param outputFile name of the analysis result file
+     * @throws Exception
+     */
+    public void start(String inputFile, String outputFile) throws Exception {
 
         ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(inputFile));
         LaLexer lexer = new LaLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
+        
         LaParser parser = new LaParser(tokens);
-             
+
         parser.removeErrorListeners();
         lexer.removeErrorListeners();
-        
+
         SaidaParser out = new SaidaParser();
         ErrorListener listener = new ErrorListener(out);
 
@@ -49,5 +52,14 @@ public class Runner {
         saida.print(out);
         saida.flush();
         saida.close();
+    }
+
+    /**
+     * Executes Runner.start() method with the arguments given.
+     * 
+     * @param args array that contains the names of the input and output files
+     */
+    public static void main(String[] args) throws Exception {
+        new Runner().start(args[0], args[1]);
     }
 }
